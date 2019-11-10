29e07eb7be24a1f2957658172c8a33d50d49673e
Darzang <mathieu.boez@gmail.com>
2019-11-10 16:18:49 +0100

    Load data on reload after saving Unity

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class GameData
+{
+ public string tilesDiscovered;
+
+ public GameData(List<GameObject> discoveredTiles)
+ {
+  foreach (GameObject tile in discoveredTiles)
+  {
+   tilesDiscovered += tile.name + "|";
+  }
+ }
+}
---
+++ Assets/Scripts/GameDataSaver.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameDataSaver : MonoBehaviour
+{
+   public string discoveredTiles;
+   public static void SaveFile(GameData gameData)
+   {
+      string path = Application.persistentDataPath + "/save.dat";
+      FileStream file;
+      if (File.Exists(path)) file = File.OpenWrite(path);
+      else file = File.Create(path);
+      BinaryFormatter bf = new BinaryFormatter();
+      bf.Serialize(file, gameData);
+      file.Close();
+   }
+   public static string LoadFile()
+   {
+      string path = Application.persistentDataPath + "/save.dat";
+      FileStream file;
+
+      if(File.Exists(path)) file = File.OpenRead(path);
+      else
+      {
+         Debug.LogError("File not found");
+         return null;
+      }
+
+      BinaryFormatter bf = new BinaryFormatter();
+      GameData data = (GameData) bf.Deserialize(file);
+
+      file.Close();
+      return data.tilesDiscovered;
+
+   }
+}
