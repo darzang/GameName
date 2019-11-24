64de23002021e63aabd99c522d59cc8a09a423d2
Darzang <mathieu.boez@gmail.com>
2019-11-24 18:47:38 +0100

    Plane with Text and onMouseDown event ok

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    private TextMeshProUGUI debugText;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        debugText = GameObject.Find("DebugText").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Click detected on" + this.gameObject.name);
+        StopCoroutine("DisplayText");
+        StartCoroutine("DisplayText");
+    }
+
+    IEnumerator DisplayText()
+    {
+        debugText.gameObject.SetActive(true);
+        debugText.text = "Clicked on " + this.gameObject.name;
+        yield return new WaitForSeconds(1f);
+        debugText.gameObject.SetActive(false);
+    }
+
+}
