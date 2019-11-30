82248e419c9374d18c3d623ab28933a40ca83d81
Darzang <mathieu.boez@gmail.com>
2019-11-30 15:22:02 +0100

    First animation to options and back

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -4,20 +4,24 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+
 public class MenuButton : MonoBehaviour
 {
+    public MenuManager menuManager;
     private TextMeshProUGUI debugText;
     // Start is called before the first frame update
     private void Awake()
     {
+        menuManager = GameObject.Find("MenuManager").GetComponent<MenuManager>();
         debugText = GameObject.Find("DebugText").GetComponent<TextMeshProUGUI>();
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Click detected on" + this.gameObject.name);
-        StopCoroutine("DisplayText");
-        StartCoroutine("DisplayText");
+        menuManager.HandleClick(this.gameObject);
+
+//        StopCoroutine("DisplayText");
+//        StartCoroutine("DisplayText");
     }
 
     IEnumerator DisplayText()
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private TextMeshPro text;
+    public GameObject player;
+    private Animation anim;
     void Start()
     {
         Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+        anim = player.GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Ray forwardRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(forwardRay, out hit, 3))
+        {
+            if (hit.collider.gameObject.tag == "MenuButton")
+            {
+                text = hit.collider.gameObject.GetComponentInChildren<TextMeshPro>();
+                text.color = Color.blue;
+            }
+            else if (text)
+            {
+                text.color = Color.yellow;
+            }
+        }
+    }
 
+    public void HandleClick(GameObject button)
+    {
+        switch (button.name)
+        {
+            case "OptionsButton":
+                anim.Play("GoToOptions");
+                break;
+            case "BackButton" :
+                anim.Play("OptionsToMain");
+                break;
+            default:
+                break;
+
+        }
     }
 }
