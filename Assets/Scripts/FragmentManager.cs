e54b2dc0617a69049fcd1861e1d0dfdf6e5f1489
Mathieu <mathieu.boez@gmail.com>
2020-01-19 19:59:30 +0100

    Random fragment done

--- Assets/Scripts/FragmentManager.cs
+++ Assets/Scripts/FragmentManager.cs
@@ -17,19 +17,21 @@ public class FragmentManager : MonoBehaviour {
         List<GameObject> availableFloorTiles = floorTiles;
         List<GameObject> availableTiles = tiles;
         List<GameObject> tilesLeadingToIncompleteFragment = new List<GameObject>();
-        
+
         // Create each fragment
         for (int i = 0; i < nbFragments; i++) {
             Debug.Log($"Creating Fragment {i + 1}");
             List<string> tilesNameInFragments = new List<string>();
             List<GameObject> tilesInFragments = new List<GameObject>();
-            
+
             // First Tile needs to be floorTile to get a spawnTile
-            List<GameObject> floorTilesLeft = availableFloorTiles.Where(floorTile => !tilesLeadingToIncompleteFragment.Find(tile => tile == floorTile)).ToList();
+            List<GameObject> floorTilesLeft = availableFloorTiles
+                .Where(floorTile => !tilesLeadingToIncompleteFragment.Find(tile => tile == floorTile)).ToList();
             if (floorTilesLeft.Count == 0) {
                 Debug.Log("No more fragment possible");
                 break;
             }
+
             GameObject firstTile = floorTilesLeft[Random.Range(0, floorTilesLeft.Count - 1)];
             availableTiles.Remove(firstTile);
             availableFloorTiles.Remove(firstTile);
@@ -46,7 +48,7 @@ public class FragmentManager : MonoBehaviour {
                     foreach (GameObject neighborTile in neighborTiles) {
                         if (!tilesInFragments.Find(t => t == neighborTile)
                             && !availableNeighborTiles.Find(t => t == neighborTile)
-                            && availableTiles.Find(t=> t == neighborTile)) {
+                            && availableTiles.Find(t => t == neighborTile)) {
                             availableNeighborTiles.Add(neighborTile);
                         }
                     }
@@ -56,18 +58,21 @@ public class FragmentManager : MonoBehaviour {
                     Debug.Log($"Fragment {i + 1} cannot be completed, no neighbor tiles for tile {j + 1}");
                     foreach (GameObject tile in tilesInFragments) {
                         availableTiles.Add(tile);
-                        if(tile.CompareTag("Floor")) availableFloorTiles.Add(tile);
+                        if (tile.CompareTag("Floor")) availableFloorTiles.Add(tile);
                     }
+
                     tilesLeadingToIncompleteFragment.Add(firstTile);
                     break;
                 }
+
                 // Get distance of each available neighborTiles
                 float distanceMax = 100f;
                 GameObject closestTile = null;
                 foreach (GameObject neighborTile in availableNeighborTiles) {
                     float currentDistance = 0;
                     foreach (GameObject fragmentTile in tilesInFragments) {
-                        currentDistance += Vector3.Distance(fragmentTile.transform.position, neighborTile.transform.position);
+                        currentDistance += Vector3.Distance(fragmentTile.transform.position,
+                            neighborTile.transform.position);
                     }
 
                     if (currentDistance < distanceMax) {
@@ -75,6 +80,7 @@ public class FragmentManager : MonoBehaviour {
                         closestTile = neighborTile;
                     }
                 }
+
                 tilesInFragments.Add(closestTile);
                 tilesNameInFragments.Add(closestTile.name);
                 availableTiles.Remove(closestTile);
@@ -82,41 +88,53 @@ public class FragmentManager : MonoBehaviour {
             }
 
             if (tilesInFragments.Count >= minFragmentSize) {
-                fragments.Add(new Fragment(tilesNameInFragments, spawnTile, i+1));
+                fragments.Add(new Fragment(tilesNameInFragments, spawnTile, i + 1));
             }
             else {
-                Debug.Log($"Not enough tile in fragment {i +1}, retrying");
+                Debug.Log($"Not enough tile in fragment {i + 1}, retrying");
                 i -= 1; // Retry to do another fragment starting from another tile
             }
         }
-        //TODO: Here add the lonely tiles
+
+        // Add the lonely tiles
+        foreach (GameObject tileLeft in availableTiles) {
+            Fragment closestFragment = GetFragmentForTile(fragments, tileLeft);
+            Fragment updatedFragment = fragments.Find(frg => frg == closestFragment);
+            if (updatedFragment != null) {
+                updatedFragment.tiles.Add(tileLeft.name);
+            }
+            else {
+                Debug.Log("Something went wrong here");
+            }
+        }
+
         int totalTilesInFragments = 0;
         foreach (Fragment fragment in fragments) {
             totalTilesInFragments += fragment.tiles.Count;
         }
-        Debug.Log($"{fragments.Count} fragments, {totalTilesInFragments} tiles covered, {availableTiles.Count} still left");
+
+        Debug.Log(
+            $"{fragments.Count} fragments, {totalTilesInFragments} tiles covered");
         return fragments;
     }
 
-    public List<Fragment> GenerateRandomFragmentsOld(List<GameObject> tiles, List<GameObject> floorTiles,
-        TileManager tileManager) {
-        int nbFragments = (int) Math.Round(tiles.Count / 15.0);
-        int tilesPerFragment = (int) Math.Round((double) (tiles.Count / nbFragments));
-        List<GameObject> availableTiles = tiles;
-        List<Fragment> fragments = new List<Fragment>();
-        List<GameObject> availableSpawnTiles = floorTiles;
-        for (int i = 0; i < nbFragments; i++) {
-            List<string> tilesInFragment = new List<string>();
-            for (int j = 0; j < tilesPerFragment; j++) {
-                GameObject tile = availableTiles[Random.Range(0, availableTiles.Count - 1)];
-                tilesInFragment.Add(tile.name);
-                availableTiles.Remove(tile);
+    public Fragment GetFragmentForTile(List<Fragment> fragments, GameObject tile) {
+        // Get distance of each available neighborTiles
+        float distanceMax = 100f;
+        Fragment closestFragment = null;
+        foreach (Fragment fragment in fragments) {
+            float currentDistance = 0;
+            foreach (string tileName in fragment.tiles) {
+                GameObject fragmentTile = GameObject.Find(tileName);
+                currentDistance += Vector3.Distance(fragmentTile.transform.position, tile.transform.position);
             }
 
-            string spawnTile = availableSpawnTiles[Random.Range(0, availableSpawnTiles.Count - 1)].name;
-            fragments.Add(new Fragment(tilesInFragment, spawnTile, i));
+            if (currentDistance < distanceMax) {
+                distanceMax = currentDistance;
+                closestFragment = fragment;
+            }
         }
 
-        return fragments;
+        return closestFragment;
     }
 }
