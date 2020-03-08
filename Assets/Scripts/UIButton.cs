3613ed5c41d0817fbc2bc9b6e323bf6c76f10ebe
Mathieu <mathieu.boez@gmail.com>
2020-03-08 10:16:32 +0100

    Commit before getting into custom controls

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -8,13 +8,12 @@ public class UIButton : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnMouseExit() {
-        Debug.Log($"MouseExit {this.gameObject.name}");
+        Debug.Log($"MouseExit {gameObject.name}");
         transform.GetComponent<TextMeshProUGUI>().fontSize -= 5;
-
     }
 
     private void OnMouseEnter() {
-        Debug.Log($"MouseEnter {this.gameObject.name}");
+        Debug.Log($"MouseEnter {gameObject.name}");
         transform.GetComponent<TextMeshProUGUI>().fontSize += 5;
     }
 }
